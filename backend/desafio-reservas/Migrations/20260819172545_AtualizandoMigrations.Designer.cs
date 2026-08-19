f9ae0a2d6db756f652794ff7887b81bc7486899b
Miltonjss <149200599+Miltonjss@users.noreply.github.com>
2026-08-19 13:28:57 -0400

    Atualizando Migrations

--- backend/desafio-reservas/Migrations/20260819172545_AtualizandoMigrations.Designer.cs
+++ backend/desafio-reservas/Migrations/20260819172545_AtualizandoMigrations.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using desafio_reservas.Data;
 
@@ -10,9 +11,11 @@ using desafio_reservas.Data;
 namespace desafio_reservas.Migrations
 {
     [DbContext(typeof(ReservasDbContext))]
-    partial class ReservasDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260819172545_AtualizandoMigrations")]
+    partial class AtualizandoMigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.19");
---
+++ backend/desafio-reservas/Migrations/20260819172545_AtualizandoMigrations.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace desafio_reservas.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizandoMigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Salas",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Salas",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
